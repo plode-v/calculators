1c2be766fad432d4b240e3d1a5a1187ee4f0706c
Plode <84858705+plode-v@users.noreply.github.com>
2023-05-14 13:04:21 +0700

    Update Home

---
+++ client/src/components/HomeIcon.tsx
@@ -0,0 +1,11 @@
+import { RiBillFill } from "react-icons/ri"
+
+const HomeIcon = ({ icon, name, divStyle, iconStyle }) => {
+    return (
+        <div className={divStyle}>
+            
+        </div>
+    )
+}
+
+export default HomeIcon
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { RiBillFill } from "react-icons/ri"
+import { HomeIcon } from "../components"
+
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="h-[90vh] flex justify-center items-center">
+            <div className="flex w-1/3 flex-wrap gap-5 justify-between">
+                <RiBillFill className="text-[100px]" />
+            </div>
+        </div>
     )
 }
 
